5c7f43c44c00d9c03b60f7cc504e0fb5d87d3921
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-13 18:42:00 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.25@998e19bbdab40aa9242aa61f86ca5aedb21f45ab 🚀

---
+++ 01-gallery.js
@@ -0,0 +1,2 @@
+import{g as o}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(o);
+//# sourceMappingURL=01-gallery.js.map
---
+++ 02-lightbox.js
@@ -0,0 +1,2 @@
+import{g as o}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(o);
+//# sourceMappingURL=02-lightbox.js.map
