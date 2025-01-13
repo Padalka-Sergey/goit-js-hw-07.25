8e9ff68039b8878acd383633b1cb796e3c77e11d
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-13 20:23:21 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.25@41b75fab4064e93abce74e3d955fa34dfa559c1c 🚀

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,2 +1,12 @@
-import{g as o}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(o);
+import{g as l}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(l);const e=document.querySelector(".gallery");console.dir(e);const a=l.map(({preview:r,original:o,description:s})=>`
+<li class="gallery__item">
+  <a class="gallery__link" href=${o}>
+    <img
+      class="gallery__image"
+      src=${r}
+      data-source="large-image.jpg"
+      alt=${s}
+    />
+  </a>
+</li>`).join("");console.log(a);e.innerHTML=a;
 //# sourceMappingURL=01-gallery.js.map
