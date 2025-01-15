2a5d09750837b94ae8b108fed98784cccc9c2e4a
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-15 21:39:02 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.25@83dc1e3000969c2f46b156a102fc3c119aefc234 🚀

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,14 +1,14 @@
-import{g as l}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */const t=document.querySelector(".gallery"),i=l.map(({preview:e,original:a,description:r})=>`
-<li class="gallery__item">
-  <a class="gallery__link" href=${a}>
-    <img
-      class="gallery__image"
-      src=${e}
-      data-source="large-image.jpg"
-      alt=${r}
-    />
+import{g as o}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */const l=document.querySelector(".gallery");let n=null,r=null;const c=o.map(({preview:e,original:t,description:i})=>`
+  <li class="gallery__item">
+  <a class="gallery__link" href=${t}>
+  <img
+  class="gallery__image"
+  src=${e}
+  data-source="large-image.jpg"
+  alt=${i}
+  />
   </a>
-</li>`).join("");t.innerHTML=i;t.addEventListener("click",n);function n(e){if(e.target.nodeName!=="IMG")return;const a=e.target.parentNode.href;basicLightbox.create(`
-    <img src=${a} width="800" height="600">
-`).show(),e.preventDefault()}
+  </li>`).join("");l.innerHTML=c;l.addEventListener("click",s);function s(e){if(e.preventDefault(),e.target.nodeName!=="IMG")return;const t=e.target.parentNode.href;n=basicLightbox.create(`
+    <img src=${t} width="800" height="600">
+    `),n.show(),d()}function d(){document.addEventListener("keydown",a),r=document.querySelector(".basicLightbox"),r.addEventListener("click",m)}function m(e){e.target===r&&document.removeEventListener("keydown",a)}function a(e){e.code==="Escape"&&n.close(()=>{document.removeEventListener("keydown",a)})}
 //# sourceMappingURL=01-gallery.js.map
