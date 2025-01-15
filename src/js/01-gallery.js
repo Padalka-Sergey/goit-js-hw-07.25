83dc1e3000969c2f46b156a102fc3c119aefc234
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-15 23:38:46 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,38 +1,64 @@
 import { galleryItems } from './gallery-items.js';
-
 // Change code below this line
 
 const listGallery = document.querySelector('.gallery');
+let instance = null;
+let lightBoxRef = null;
 
 const markup = galleryItems
   .map(
     ({ preview, original, description }) => `
-<li class="gallery__item">
+  <li class="gallery__item">
   <a class="gallery__link" href=${original}>
-    <img
-      class="gallery__image"
-      src=${preview}
-      data-source="large-image.jpg"
-      alt=${description}
-    />
+  <img
+  class="gallery__image"
+  src=${preview}
+  data-source="large-image.jpg"
+  alt=${description}
+  />
   </a>
-</li>`
+  </li>`
   )
   .join('');
 
 listGallery.innerHTML = markup;
 
-listGallery.addEventListener('click', openLargeImage);
+listGallery.addEventListener('click', openModalImageHandler);
 
-function openLargeImage(evt) {
+function openModalImageHandler(evt) {
+  evt.preventDefault();
   if (evt.target.nodeName !== 'IMG') {
     return;
   }
   const parentEl = evt.target.parentNode.href;
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
     <img src=${parentEl} width="800" height="600">
-`);
+    `);
 
   instance.show();
-  evt.preventDefault();
+  makeListeners();
+}
+
+function makeListeners() {
+  document.addEventListener('keydown', removeListenerEscHandler);
+  lightBoxRef = document.querySelector('.basicLightbox');
+  lightBoxRef.addEventListener('click', clickModalDelListenerHandler);
+}
+
+function clickModalDelListenerHandler(evt) {
+  // console.log(evt.target);
+  if (evt.target !== lightBoxRef) {
+    return;
+  }
+  document.removeEventListener('keydown', removeListenerEscHandler);
+}
+
+function removeListenerEscHandler(evt) {
+  // console.log('Слушатель ESC');
+  if (evt.code !== 'Escape') {
+    return;
+  }
+  instance.close(() => {
+    document.removeEventListener('keydown', removeListenerEscHandler);
+  });
 }
