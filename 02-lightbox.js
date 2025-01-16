c605a9b46361486b1d572886f478b7fe50d9f794
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-01-16 21:45:17 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-07.25@ffacb6b330b0ce60a4e638c0439e466bc1918f5d 🚀

--- 02-lightbox.js
+++ 02-lightbox.js
@@ -1,2 +1,11 @@
-import{g as o}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(o);
+import{g as l}from"./assets/gallery-items-C3TQHNc4.js";/* empty css                      */console.log(l);const a=document.querySelector(".some-element"),t=l.map(({preview:e,original:n,description:r})=>`
+  <li class="gallery__item">
+  <a class="gallery__link" href=${n}>
+  <img
+  class="gallery__image"
+  src=${e}
+  alt=${r}
+  />
+  </a>
+  </li>`).join("");a.innerHTML=t;a.addEventListener("click",o);function o(e){e.preventDefault(),e.target.nodeName==="IMG"&&new SimpleLightbox(".some-element a")}
 //# sourceMappingURL=02-lightbox.js.map
